02f8c9893186188617deacc71b42d819751beab3
Christian Plesner Hansen <c7n@p5r.org>
2015-03-31 13:47:09 +0200

    Working on the windows machines.

--- vagrant/common/common.rb
+++ vagrant/common/common.rb
@@ -20,43 +20,83 @@ module Common
     end
 
     # Returns a field from the vm spec associated with this vm.
-    def vm_field(name, default=nil)
+    def shared_vm_field(name, default=nil)
       VMS_DICT.fetch("vms").fetch(@id).fetch(name, default)
     end
 
+    # Returns a field from the local vm overrides.
+    def local_vm_field(name, default=nil)
+      LOCAL_DICT.fetch("vms", {}).fetch(@id, {}).fetch(name, default)
+    end
+
+    # Returns the value of the vm field with the given name, taking local vm
+    # overrides into account.
+    def vm_field(name, default=nil)
+      self.local_vm_field(name, self.shared_vm_field(name, default))
+    end
+
     # Returns a field from the common spec.
-    def common_field(name, default=nil)
+    def shared_common_field(name, default=nil)
       VMS_DICT.fetch("common", {}).fetch(name, default)
     end
 
-    # Returns a local override for the given name.
-    def local_field(name, default=nil)
+    # Returns a local common override for the given name.
+    def local_common_field(name, default=nil)
       LOCAL_DICT.fetch("common", {}).fetch(name, default)
+    end
+
+    # Returns the value of the common field with the given name, taking local
+    # overrides into account.
+    def common_field(name, default=nil)
+      self.local_common_field(name, self.shared_common_field(name, default))
+    end
+
+    def shared_os_field(name, default=nil)
+      VMS_DICT.fetch(self.os, {}).fetch(name, default)
+    end
+
+    def local_os_field(name, default=nil)
+      LOCAL_DICT.fetch(self.os, {}).fetch(name, default)
+    end
+
+    def os_field(name, default=nil)
+      self.local_os_field(name, self.shared_os_field(name, default))
     end
 
     # Returns the host port onto which the vm's ssh port should be mapped.
     def ssh_port
-      self.vm_field("ssh_port")
+      self.nearest_field "ssh_port"
+    end
+
+    def os
+      self.vm_field "os"
+    end
+
+    def gui
+      self.nearest_field("gui", false)
     end
 
     # Returns the path where vagrant should look for local boxes.
     def local_box_path name
-      boxes_root = (self.local_field "boxes_path") || ENV['VAGRANT_BOXES'] || "~/.boxes"
+      boxes_root = (self.nearest_field "boxes_path") || ENV['VAGRANT_BOXES'] || "~/.boxes"
       "#{boxes_root}/#{name}"
     end
 
     # Invokes the given block for each entry of the field with the given name
     # in the union of common, local, and vm-specific configs.
     def each_field(name, &thunk)
-      self.common_field(name, []).each &thunk
-      self.vm_field(name, []).each &thunk
-      self.local_field(name, []).each &thunk
+      self.local_common_field(name, []).each &thunk
+      self.shared_common_field(name, []).each &thunk
+      self.local_os_field(name, []).each &thunk
+      self.shared_os_field(name, []).each &thunk
+      self.local_vm_field(name, []).each &thunk
+      self.shared_vm_field(name, []).each &thunk
     end
 
     # Yields the field with the given name from the nearest config in the order
     # local overrides < vm settings < common defaults < explicit default.
     def nearest_field(name, default=nil)
-      (self.local_field name) || (self.vm_field name) || (self.common_field name) || default
+      self.vm_field(name, self.os_field(name, self.common_field(name, default)))
     end
 
     # Yields the path to the private key to use.
@@ -93,16 +133,36 @@ module Common
         host: self.ssh_port,
         guest: 22,
         id: "ssh"
+      config.vm.provider :virtualbox do |vb|
+        vb.gui = self.gui
+      end
     end
 
   end
 
+  # Apply the default configuration based on the given id. If a thunk is passed
+  # it will be called with the vagrant config and the externs object which can
+  # be used to override the default settings.
   def self.configure(id, &thunk)
     externs = Externs.new id
+    default_box = externs.local_box_path ("%s.box" % id)
     Vagrant.configure("2") do |config|
       config.vm.box = id
-      thunk.call(config, externs)
+      config.vm.box_url = default_box
+
       externs.add_common_configs config
+
+      thunk.call(config, externs) unless thunk.nil?
+    end
+  end
+
+  # Works the same way as .configure but sets up additional options for running
+  # a windows box.
+  def self.configure_windows(id, &thunk)
+    self.configure(id) do |config, externs|
+      config.vm.communicator = :winrm
+
+      thunk.call(config, externs) unless thunk.nil?
     end
   end
 
--- vagrant/windows/start-jenkins-slave.bat
+++ vagrant/windows/start-jenkins-slave.bat
@@ -1,5 +1,1 @@
-REM sleep to give the network share time to become available.
-timeout 60
-
-REM delegate to the real runner.
-C:\platform\windows\run-jenkins-slave.bat 1> C:\Users\vagrant\Documents\jenkins\jenkins-slave.out 2> C:\Users\vagrant\Documents\jenkins\jenkins-slave.err
+C:\Users\vagrant\Jenkins\run-jenkins-slave.bat 1> C:\Users\vagrant\Jenkins\jenkins-slave.out 2> C:\Users\vagrant\Jenkins\jenkins-slave.err
