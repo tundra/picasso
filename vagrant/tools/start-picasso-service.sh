8d09b9ff21ccb4ae29541814ea63a5357ee8d447
Christian Plesner Hansen <c7n@p5r.org>
2015-10-20 11:28:29 +0200

    Various changes

--- vagrant/tools/start-picasso-service.sh
+++ vagrant/tools/start-picasso-service.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -f /home/plesner/nopicasso ]; then
+  echo "Picasso disabled."
+  exit 0
+fi
+
 TOOLS="$(dirname $0)"
 
 $TOOLS/foreach.sh up > /tmp/picasso.out 2> /tmp/picasso.err 
