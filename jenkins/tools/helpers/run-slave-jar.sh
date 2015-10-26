f4e199cbe744279cb06b8df17a997cde1de75ed6
Christian Plesner Hansen <plesner@t.undra.org>
2015-10-26 08:43:54 +0100

    Set library container

--- jenkins/tools/helpers/run-slave-jar.sh
+++ jenkins/tools/helpers/run-slave-jar.sh
@@ -4,6 +4,9 @@ set -e
 
 cd /home/vagrant
 
+# Set up the build environment.
+export LIBRARY_CONTAINER=/storage/libraries/
+
 # Start the slave agent.
 java                                                                           \
   -jar slave.jar                                                               \
